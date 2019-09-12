c3c1910635a14e7da1d7900f03b5a3380f6d2a09
AlekseyDvornikov1 <lesha.dvornikov1@gmail.com>
2019-09-12 07:02:47 +0300

    + PostController

---
+++ app/Http/Controllers/Blog/Admin/PostController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers\Blog\Admin;
+
+use App\Repositories\BlogCategoryRepository;
+use App\Repositories\BlogPostRepository;
+use Illuminate\Http\Request;
+
+/**
+ * Управление статьями блога
+ * Class PostController
+ * @package App\Http\Controllers\Blog\Admin
+ */
+class PostController extends BaseController
+{
+    /**
+     * @var BlogPostRepository
+     */
+    private $blogPostRepository;
+
+    /**
+     * @var BlogCategoryRepository
+     */
+    private $blogCategoryRepository;
+
+    /**
+     * PostController constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->blogPostRepository = app(BlogPostRepository::class);
+        $this->blogCategoryRepository = app(BlogCategoryRepository::class);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $paginator = $this->blogPostRepository->getAllWithPaginate(25);
+
+        return view('blog.admin.posts.index',compact('paginator'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        dd(1);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $post = $this->blogPostRepository->getEdit($id);
+        if(empty($post)) {
+            abort(404);
+        }
+
+        $categoryList = $this->blogCategoryRepository->getComboBox();
+
+        return view('blog.admin.posts.edit',compact('post','categoryList'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        dd(1);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
